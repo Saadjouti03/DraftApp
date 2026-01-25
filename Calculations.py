9f60072f7a38cbc47c367d2b0b0757ed67e8621c
saadjouti03 <Saadjouti2@gmail.com>
2026-01-25 18:04:39 -0500

    initial commit

---
+++ Calculations.py
@@ -0,0 +1,21 @@
+# Calculations
+answer = 5 * 6
+print("The answer to your query is:", answer)
+
+answer = 8 / 3
+print("The answer to your query is:", answer)
+
+answer = 8 // 3
+print("The answer to your query is:", answer)
+
+answer = 8 % 3
+print("The answer to your query is:", answer)
+
+answer = 4200000 + 23090
+print("The answer to your query is:", answer)
+
+answer = 4200000 - 7633
+print("The answer to your query is:", answer)
+
+# Print name
+print("Saad Jouti")
